299fc88ff6d9182e86b484c9507afa318ac561a2
Mariano Gappa <spinetta@gmail.com>
2019-08-26 18:09:55 +0100

    https://www.hackerrank.com/challenges/sock-merchant

---
+++ challenges/sock-merchant/main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the sockMerchant function below.
+func sockMerchant(n int32, ar []int32) int32 {
+	// Get the total number of socks per color
+	colors := map[int]int{}
+	for _, c := range ar {
+		colors[int(c)]++
+	}
+
+	// Sum the total of pairs per color
+	totalPairs := 0
+	for _, c := range colors {
+		totalPairs += c / 2
+	}
+
+	return int32(totalPairs)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	arTemp := strings.Split(readLine(reader), " ")
+
+	var ar []int32
+
+	for i := 0; i < int(n); i++ {
+		arItemTemp, err := strconv.ParseInt(arTemp[i], 10, 64)
+		checkError(err)
+		arItem := int32(arItemTemp)
+		ar = append(ar, arItem)
+	}
+
+	result := sockMerchant(n, ar)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
