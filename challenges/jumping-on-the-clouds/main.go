4ca696ab250ab73bf82847cb6d12973ea52ecb31
Mariano Gappa <spinetta@gmail.com>
2019-08-26 18:58:26 +0100

    https://www.hackerrank.com/challenges/jumping-on-the-clouds

---
+++ challenges/jumping-on-the-clouds/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// Complete the jumpingOnClouds function below.
+// Time: O(n)
+// Space: O(1)
+func jumpingOnClouds(c []int32) int32 {
+	jumpCount := 0
+	for i := 0; i < len(c)-1; i++ {
+		jumpCount++
+		if i+2 >= len(c) || (i+2 < len(c) && c[i+2] == 0) {
+			i++
+			continue
+		}
+	}
+	return int32(jumpCount)
+}
+
+func main() {
+	reader := bufio.NewReaderSize(os.Stdin, 1024*1024)
+
+	stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+	checkError(err)
+
+	defer stdout.Close()
+
+	writer := bufio.NewWriterSize(stdout, 1024*1024)
+
+	nTemp, err := strconv.ParseInt(readLine(reader), 10, 64)
+	checkError(err)
+	n := int32(nTemp)
+
+	cTemp := strings.Split(readLine(reader), " ")
+
+	var c []int32
+
+	for i := 0; i < int(n); i++ {
+		cItemTemp, err := strconv.ParseInt(cTemp[i], 10, 64)
+		checkError(err)
+		cItem := int32(cItemTemp)
+		c = append(c, cItem)
+	}
+
+	result := jumpingOnClouds(c)
+
+	fmt.Fprintf(writer, "%d\n", result)
+
+	writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+	str, _, err := reader.ReadLine()
+	if err == io.EOF {
+		return ""
+	}
+
+	return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
